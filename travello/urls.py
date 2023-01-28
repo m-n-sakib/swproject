d4d2c5a13248ec6b95e376c39843f9ea7343f341
m-n-sakib <u1704004@student.cuet.ac.bd>
2023-01-28 20:50:51 +0600

    https://github.com/mahedi-hasan-75/tourism?fbclid=IwAR13sgUEdy5aZ4GQU3_D6rLq4ZKYIoLLK8txXk3bHOs4rqEd0wksWdqwp6s

--- travello/urls.py
+++ travello/urls.py
@@ -3,11 +3,12 @@ from django.urls import path, include
 
 from . import views
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index', views.index, name='index'),
     path('register', views.register, name='register'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('search', views.search, name='search'),
+    
     path('destination_list/<str:city_name>', views.destination_list, name='destination_list'),
     path('destination_list/destination_details/<str:city_name>', views.destination_details, name='destination_details'),
     path('destination_details/<str:city_name>', views.destination_details, name='destination_details'),
